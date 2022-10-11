fc8949f35342fc971e8c7a330941faeb6c04d904
archy <archy@yeet.sus>
2022-10-11 13:17:13 +0200

    Code cleaning

--- src/inss-rec.cpp
+++ src/inss-rec.cpp
@@ -3,21 +3,14 @@ using namespace std;
 #include "util.hpp"
 
 void insertionRecursionBody(vector<TYPE>& v, int len, int idx){
-	int i,j;
-	if(len<0){
-		i = -len;
-		j = i+1;
+	if(len<=0){
+		int i = -len;
+		int j = i+1;
 		if(cmp(v,i,j)<0){ swap(v,i,j);}
-		if(j>0){ insertionRecursionBody(v,len+1,-1);}
-	}else if(len > 0){
-		if(len >0 && idx < len){
+		if(j>0 && len != 0){ insertionRecursionBody(v,len+1,-1);}
+	}else if(idx < len){
 			insertionRecursionBody(v,-idx,-1);
 			insertionRecursionBody(v,len,idx+1);
-		}
-	}else{
-		i = len;
-		j = i+1;
-		if(cmp(v,i,j)<0){ swap(v,i,j);}
 	}
 }
 
